5c3dc869e3bd6a12744611b92fcb0bac4409c3cd
Sarang Amborkar <sarang.amborkar@niyuj.com>
2022-01-11 19:05:46 +0530

    Configuration files

---
+++ config.py
@@ -0,0 +1,26 @@
+"""
+ The config module contains the Config class, which is passed to
+the Welkin V8 API calls. It stores useful information such as your API client name, tenant name,
+instance name, secret and environment(stg/live).
+
+It is suggested that you create a single Config object in
+your project and pass that to the various API calls, rather than create new
+Config objects. This is merely a design suggestion, treat it as such.
+"""
+from __future__ import absolute_import
+
+
+class Config(object):
+    """common configuration"""
+
+    # organization (Tenant)
+    tenantName = ""
+    # API Client name
+    apiClientName = ""
+    # Instance (Environment)
+    instanceName = ""
+    SECRET = ""
+    # stg/live
+    ENV = ""
+
+
