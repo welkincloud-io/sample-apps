f8d13a870c22bc04e41b39a3c7b89e253b05ea81
Sarang Amborkar <sarang.amborkar@niyuj.com>
2022-01-24 17:44:23 +0530

    Create patient

--- scripts/create_patient.py
+++ scripts/create_patient.py
@@ -12,24 +12,25 @@ Example:
 import requests
 import json
 from dotenv import get_key
-from config import Config as C
+from config import Config as c
 
-token = get_key('../.env','WELKIN_API_TOKEN')
-headers = { "Authorization": "Bearer {}".format(token)}
+token = get_key('../.env', 'WELKIN_API_TOKEN')
+headers = {"Authorization": "Bearer {}".format(token)}
 
 
-def create_patient(data):
-  url = f"https://api.{C.ENV}.welkincloud.io/{C.tenantName}/{C.instanceName}/patients"
-  print(url)
-  r = requests.post(url, headers=headers, json=data)
+def create_patient(json_data):
+    url = f"https://api.{c.ENV}.welkincloud.io/{c.tenantName}/{c.instanceName}/patients"
+    print(url)
+    r = requests.post(url, headers=headers, json=json_data)
 
-  if r.ok:
-    print("Patient Created Successfully")
-    print('response', r)
-  else:
-    print(r.status_code, r.text)
+    if r.ok:
+        print("Patient Created Successfully")
+        print(r)
+        print(r.json())
+    else:
+        print(r.status_code, r.text)
 
 
 if __name__ == '__main__':
-  data = json.load(open('create_patient.json'))
-  create_patient(data)
+    data = json.load(open('create_patient.json'))
+    create_patient(data)
