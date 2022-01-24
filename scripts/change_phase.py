e8bdb689d07207af645d80dbbe97239235e1ed6c
Sarang Amborkar <sarang.amborkar@niyuj.com>
2022-01-24 18:52:22 +0530

    Change Program Phase for a Patient

--- scripts/change_phase.py
+++ scripts/change_phase.py
@@ -13,33 +13,32 @@ Example:
   python change_phase.py 14450e16-3d7d-4d2c-b993-384f2f2279e7 prog-sample
 """
 
-from __future__ import absolute_import
 import requests
 from docopt import docopt
 import json
 from dotenv import get_key
-from config import Config
+from config import Config as c
 
-token = get_key('../.env','WELKIN_API_TOKEN')
-headers = { "Authorization": "Bearer {}".format(token)}
+token = get_key('../.env', 'WELKIN_API_TOKEN')
+headers = {"Authorization": "Bearer {}".format(token)}
 
 
 def change_phase(patientId, programName):
     data = json.load(open('change_phase.json'))
-    print('data', data)
-    url = 'https://api.%s.welkincloud.io/%s/%s/patients/%s/programs/%s/phases' \
-        %(Config.ENV, Config.tenantName, Config.instanceName, patientId, programName)
-    print('URL', url)
+    url = f'https://api.{c.ENV}.welkincloud.io/{c.tenantName}/{c.instanceName}' \
+          f'/patients/{patientId}/programs/{programName}/phases'
+
+    print(url)
     r = requests.patch(url, headers=headers, json=data)
     if r.ok:
-        print('Patient Assigned to "%s" Phase successfully.....' %data['phaseName'])
-        print('response', r)
+        print('Patient Assigned to Phase successfully.....')
+        print('response', r.json())
     else:
         print(r.status_code, r.text)
 
 
 if __name__ == '__main__':
     args = docopt(__doc__)
-    patientId  = args.get('<patientId>')
+    patientId = args.get('<patientId>')
     programName = args.get('<programName>')
     change_phase(patientId, programName)
