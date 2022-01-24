fb4382b9eef3d592fa6b2825a2ad5a21ebb0644a
Sarang Amborkar <sarang.amborkar@niyuj.com>
2022-01-24 18:51:20 +0530

    Assign a patient to Program

--- scripts/assign_to_program.py
+++ scripts/assign_to_program.py
@@ -17,29 +17,28 @@ import json
 import requests
 from docopt import docopt
 from dotenv import get_key
-from config import Config
+from config import Config as c
 
-token = get_key('../.env','WELKIN_API_TOKEN')
-headers = { "Authorization": "Bearer {}".format(token)}
+token = get_key('../.env', 'WELKIN_API_TOKEN')
+headers = {"Authorization": "Bearer {}".format(token)}
 
 
 def assign_to_program(patientId, programName):
     data = json.load(open('assign_to_program.json'))
-    print('data', data)
-    url = 'https://api.%s.welkincloud.io/%s/%s/patients/%s/programs/%s' \
-        %(Config.ENV, Config.tenantName, Config.instanceName, patientId, programName)
-    print('URL', url)
+    url = f'https://api.{c.ENV}.welkincloud.io/{c.tenantName}/{c.instanceName}' \
+          f'/patients/{patientId}/programs/{programName}'
+    print(url)
     r = requests.patch(url, headers=headers, json=data)
 
     if r.ok:
         print("Patient Assigned to program Successfully .....")
-        print('response', r)
+        print('response', r.json())
     else:
         print(r.status_code, r.text)
 
 
 if __name__ == '__main__':
     args = docopt(__doc__)
-    patientId  = args.get('<patientId>')
+    patientId = args.get('<patientId>')
     programName = args.get('<programName>')
     assign_to_program(patientId, programName)
