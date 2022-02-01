2ff0b03db41b5672ef8409f458257c11b13a68f8
Sarang Amborkar <sarang.amborkar@niyuj.com>
2022-02-01 19:07:44 +0530

    Assign patient to a program

--- patient/assign_to_program.py
+++ patient/assign_to_program.py
@@ -6,17 +6,21 @@ Usage:
   assign_to_program.py [<patientId>] [<programName>]
 
 Options:
-  <patientId>                 : (string) ID of the patient
-  <programName>                    : name of the program
+  <patientId>            : (string) ID of the patient
+  <programName>          : name of the program
 
 Example:
   python assign_to_program.py 14450e16-3d7d-4d2c-b993-384f2f2279e7 prog-sample
 """
 
+import os
+import sys
 import json
 import requests
 from docopt import docopt
 from dotenv import get_key
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import Config as c
 
 token = get_key('../.env', 'WELKIN_API_TOKEN')
@@ -24,7 +28,7 @@ headers = {"Authorization": "Bearer {}".format(token)}
 
 
 def assign_to_program(patientId, programName):
-    data = json.load(open('assign_to_program.json'))
+    data = json.load(open('patient/assign_to_program.json'))
     url = f'https://api.{c.ENV}.welkincloud.io/{c.tenantName}/{c.instanceName}' \
           f'/patients/{patientId}/programs/{programName}'
     print(url)
