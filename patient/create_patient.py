5d5ef0bfb9d853f19ad90575bb39c354ff89a04e
Sarang Amborkar <sarang.amborkar@niyuj.com>
2022-02-01 18:31:59 +0530

    Script for Creating a patient

--- patient/create_patient.py
+++ patient/create_patient.py
@@ -1,17 +1,21 @@
 """
 Purpose:
-  Creating a new Patient
-
+  Creating a new Patient. Before running this script
+  edit create_patient.json file add patient information in  json file.
 Usage:
   create_patient.py
 
 Example:
-  python change_phase.py 14450e16-3d7d-4d2c-b993-384f2f2279e7 prog-sample
+  python create_patient.py
 """
 
+import os
+import sys
 import requests
 import json
 from dotenv import get_key
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import Config as c
 
 token = get_key('../.env', 'WELKIN_API_TOKEN')
@@ -32,5 +36,5 @@ def create_patient(json_data):
 
 
 if __name__ == '__main__':
-    data = json.load(open('create_patient.json'))
+    data = json.load(open('patient/create_patient.json'))
     create_patient(data)
