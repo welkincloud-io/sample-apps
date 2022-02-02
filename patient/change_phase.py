327dd156201e9bcc7baa4c84dfafe76baee20397
Sarang Amborkar <sarang.amborkar@niyuj.com>
2022-02-02 18:01:04 +0530

    Change phase for patient already in program

--- patient/change_phase.py
+++ patient/change_phase.py
@@ -10,7 +10,7 @@ Options:
   <programName>                    : name of the program
 
 Example:
-  python change_phase.py 14450e16-3d7d-4d2c-b993-384f2f2279e7 prog-sample
+  python3 change_phase.py 14450e16-3d7d-4d2c-b993-384f2f2279e7 prog-sample
 """
 
 import os
